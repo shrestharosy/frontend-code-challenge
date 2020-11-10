d33a061f2cdbea7292b261cc06b17330322e1315
Rosy <rosy@fusemachines.com>
2020-11-10 19:27:01 +0545

    style : Remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Global } from "@emotion/core";
 import * as AppStyles from "./styles";
 import { GlobalStyle } from "styles/Global";
 import Sidebar from "./components/Sidebar";
-import Main from "./components/Main";
 import Dashboard from "./routes/Dashboard";
 
 function App() {
@@ -13,9 +12,7 @@ function App() {
       <Global styles={GlobalStyle} />
       <AppStyles.App>
         <Sidebar />
-        <Main>
-          <Dashboard />
-        </Main>
+        <Dashboard />
       </AppStyles.App>
     </>
   );
--- src/components/Main/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Wrapper = styled.div`
-  height: 100%;
-  width: 100%;
-`;
