80a99076f98c443f2e9595ba6fc4697eb1bd3aac
Rosy <rosy@fusemachines.com>
2020-11-11 17:59:22 +0545

    fix : Fix margin of gym card container

--- src/routes/Dashboard/GymCards/styles.ts
+++ src/routes/Dashboard/GymCards/styles.ts
@@ -1,15 +1,19 @@
 import styled from "@emotion/styled";
 
 import { mq } from "styles/media-queries";
-import { md } from "./../../../constants";
+import { sm, md } from "./../../../constants";
 
 export const GymCardsContainer = styled.div`
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
 
-  ${mq(md)} {
-    margin: -56px 0px 0px 60px;
+  ${mq(sm)} {
+    margin-left: 60px;
     padding: 0px 60px 0px 223px;
+  }
+
+  ${mq(md)} {
+    margin-top: -56px;
   }
 `;
