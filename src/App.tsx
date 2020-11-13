533a4cad7c17f645343df46efdb308cfb77c2fae
rosy <rosyshrestha028@gmail.com>
2020-11-13 09:04:35 +0545

    fix : Use hash router for routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import * as AppStyles from "./styles";
 import { GlobalStyle } from "styles/Global";
 import Sidebar from "./components/Sidebar";
 import MainRouters from "routers";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyle} />
       <AppStyles.App>
-        <BrowserRouter>
+        <HashRouter basename="/">
           <Sidebar />
           <MainRouters />
-        </BrowserRouter>
+        </HashRouter>
       </AppStyles.App>
     </>
   );
